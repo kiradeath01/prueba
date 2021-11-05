6b024f63c61aed8430ecb8b02b619fe2271c144b
Jose Maria <46497635+kiradeath01@users.noreply.github.com>
2021-11-04 21:52:00 -0600

    Add files via upload

---
+++ tienda.php
@@ -0,0 +1,269 @@
+<!--  
+      Autor: J. Manuel 
+      Para: FOMA en colaboración de cocuyodev.com
+      correo: jesusmanueldl@gmail.com
+-->
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" type="text/css" href="css/slick/slick.css"/>  
+    <link rel="stylesheet" type="text/css" href="css/slick/slick-theme.css"/> 
+
+  
+
+   
+    
+
+    <title>FOMA</title>
+  </head>
+  <body style="background: #F7F7F7;">
+    
+
+    <div class="container-fluid">
+      <div class="row" id="menu"> 
+      </div>
+      
+
+      <div class="row py-2">
+        <div class="col-md-8">
+          <div class="card shadow-sm rounded-3">
+            <div class="card-body p-3">
+              <div class="row"> 
+                <!-- galeria de miniaturas-->
+                <div class="col-md-2">
+                  <div id="gal1">
+                    <a href="#" data-image="img/fogon tradicional/imagen fogon 1.png" data-zoom-image="img/fogon tradicional/imagen fogon 1.png">
+                          <img id="img_01" class="img-thumbnail mb-2" src="img/fogon tradicional/imagen fogon 1.png"/>
+                      </a>
+
+                      <a href="#" data-image="img/small/image1.jpg" data-zoom-image="img/large/image1.jpg">
+                          <img id="img_01" class="img-thumbnail mb-2" src="img/thumb/image1.jpg"/>
+                      </a>
+
+                      <a href="#" data-image="img/small/image2.jpg" data-zoom-image="img/large/image2.jpg">
+                          <img id="img_01" class="img-thumbnail mb-2" src="img/thumb/image2.jpg"/>
+                      </a>
+
+                      <a href="#" data-image="img/small/image3.jpg" data-zoom-image="img/large/image3.jpg">
+                          <img id="img_01" class="img-thumbnail mb-2" src="img/thumb/image3.jpg"/>
+                      </a>
+
+                      <a href="#" data-image="img/small/image4.jpg" data-zoom-image="img/large/image4.jpg">
+                          <img id="img_01" class="img-thumbnail mb-2" src="img/thumb/image4.jpg"/>
+                      </a>
+                  </div> 
+                </div>
+                <!-- imagen principal-->
+                <div class="col-md-10">                
+                   <img id="zoom_03" src="img/fogon tradicional/imagen fogon 1.png" class="img-fluid" alt="..." >
+                </div>
+
+                <div class="col-md-12">
+                  <br>
+                  <hr class="text-muted">          
+                </div>
+
+                <div class="col-md-12 text-center">
+                  <h2>Otros productos</h2>
+
+                  <div class="row">
+                    <div class="card my-2 border-light">
+                      <div class="card-body">
+                   
+                        <div class="autoplay ">
+                          <div class="d-flex justify-content-center p-1">
+                            <div class="col p-0 m-0 text-center">
+                               <img class="rounded img-fluid " src="img/small/image2.jpg" alt="Third slide"> 
+                               <p style="font-size:11px;"><strong>Estufas</strong></p>    
+                            </div>  
+                          </div>
+
+                          <div class="d-flex justify-content-center p-1">
+                            <div class="col p-0 m-0 text-center">
+                               <img class="rounded img-fluid " src="img/small/image4.jpg" alt="Third slide"> 
+                               <p style="font-size:11px;"><strong>Estufas</strong></p>    
+                            </div>  
+                          </div>
+
+                          <div class="d-flex justify-content-center p-1">
+                            <div class="col p-0 m-0 text-center">
+                               <img class="rounded img-fluid " src="img/small/image3.jpg" alt="Third slide"> 
+                               <p style="font-size:11px;"><strong>Estufas</strong></p>    
+                            </div>  
+                          </div>
+
+                          <div class="d-flex justify-content-center p-1">
+                            <div class="col p-0 m-0 text-center">
+                               <img class="rounded img-fluid " src="img/small/image1.jpg" alt="Third slide"> 
+                               <p style="font-size:11px;"><strong>Estufas</strong></p>    
+                            </div>  
+                          </div>
+
+                          <div class="d-flex justify-content-center p-1">
+                            <div class="col p-0 m-0 text-center">
+                               <img class="rounded img-fluid " src="img/small/image4.jpg" alt="Third slide"> 
+                               <p style="font-size:11px;"><strong>Estufas</strong></p>    
+                            </div>  
+                          </div>
+
+                          <div class="d-flex justify-content-center p-1">
+                            <div class="col p-0 m-0 text-center">
+                               <img class="rounded img-fluid " src="img/small/image3.jpg" alt="Third slide"> 
+                               <p style="font-size:11px;"><strong>Estufas</strong></p>    
+                            </div>  
+                          </div>
+
+                        </div>
+                                           
+                      </div>
+                    </div> 
+                                    
+                  </div> 
+
+                  <div class="row mt-5 p-3">
+                     <h2>Especifiaciones del producto</h2>
+
+                    <table class="table table-borderles">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">First</th>
+                          <th scope="col">Last</th>
+                          <th scope="col">Handle</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td>Mark</td>
+                          <td>Otto</td>
+                          <td>@mdo</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">2</th>
+                          <td>Jacob</td>
+                          <td>Thornton</td>
+                          <td>@fat</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">3</th>
+                          <td colspan="2">Larry the Bird</td>
+                          <td>@twitter</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    
+                  </div>
+
+                  <h2>Otros productos</h2>
+                          
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+          <!-- descripcion del producto-->
+          <div class="col-md-4 px-4">
+            <div class="row">
+              <div class="card shadow-sm">
+                <div class="card-body ">
+                  <p class="text-muted">Nuevo | 6 Vendidos</p>
+                 <h5 class="card-title">Fogon estufa de hornillas x 1</h5>
+                 <p><del>$ 3,500</del></p>
+                 <p> <span class="display-4">$ 2,500<sup class="text-success h3"> Ahorra 10% <br> <p class="text-muted text-black-50 ">IVA incluido</p></sup></span></p>
+                 <p><span class="badge bg-primary">OFERTA DEL DÍA</span></p>
+                 <p><i class="bi bi-truck h4"></i> Envio a todo el país <br><span class="text-muted">Puedes calcular el tiempo de llegada</span></p>
+
+                 <select class="form-select mb-4" aria-label="Default select example">
+                  <option selected>Colores disponibles</option>
+                  <option value="1">Negro</option>
+                  <option value="2">Rojo</option>
+                  <option value="3">Verde</option>
+                </select>
+
+                <p>Stock disponible <b>356 unidades</b></p>
+
+                <select class="form-select mb-4" aria-label="Default select example">
+                  <option selected>Cantidad</option>
+                  <option value="1">1 Unidad</option>
+                  <option value="2">2 Unidades</option>
+                  <option value="3">3 Unidades</option>
+                </select>
+               
+                <div class="row d-flex justify-content-center">
+                 <button type="button" class="btn btn-primary btn-lg mb-2">Comprar ahora</button>
+                 <button type="button" class="btn btn-outline-info btn-lg mb-2">Agregar al carrito</button>
+                </div>
+                 
+                </div>
+              </div>
+
+              <div class="card p-3 my-3 shadow-sm">
+                <div class="card-body">
+                  <h5 class="card-title">Información del vendedor</h5>
+                </div>
+              </div>
+
+
+            </div>            
+          </div>       
+            
+      </div>
+
+    </div>
+   
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/igorlino/elevatezoom-plus@1.2.3/src/jquery.ez-plus.js"></script>
+    <script src="js/menu.js"></script>
+    <script type="text/javascript" src="css/slick/slick.min.js"></script>
+
+    <script type="text/javascript">
+      $('.autoplay').slick({
+            
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+      });
+    </script>
+
+    <script>
+      $('#zoom_03').ezPlus({
+        gallery: 'gal1', 
+        cursor: 'pointer', 
+        galleryActiveClass: 'active',
+        imageCrossfade: false,
+      //  zoomType: 'lens',
+       //containLensZoom: true,
+        loadingIcon: 'http://www.elevateweb.co.uk/spinner.gif'
+      });
+
+      $("#zoom_03").bind("click",function (e) {
+        var ez = $('#zoom_03').data('ezPlus');
+        $.fancyboxPlus(ez.getGalleryList());
+        return false;
+      });
+
+    </script>
+   
+
+   
+  </body>
+</html>
