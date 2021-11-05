03928b2647348d36c5cac699b45e947bda4a8f64
Jose Maria <46497635+kiradeath01@users.noreply.github.com>
2021-11-04 21:50:06 -0600

    Add files via upload

---
+++ carrito.php
@@ -0,0 +1,220 @@
+<!--  
+      Autor: J. Manuel 
+      Para: FOMA en colaboración de cocuyodev.com
+      correo: jesusmanueldl@gmail.com
+-->
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+    <title>FOMA</title>
+  </head>
+  <body style="background: #F7F7F7;">
+    
+
+    <div class="container-fluid">
+
+      <div class="row" id="menu"> 
+      </div>
+
+      <div class="row">
+        
+        <div class="col-md-8 my-3"> 
+        <div class="card">
+          <div class="card-header rounded-0" style="background: #76C7CB;">
+           <h4 class="text-white">Artículos en su carrito</h4>
+          </div>
+        </div>         
+
+         <!--producto-->
+          <div class="card shadow-sm my-2 rounded-3">
+            <div class="card-body py-1">
+              <div class="row d-flex align-items-center">
+                <div class="col-md-4">
+                  <img class="rounded img-fluid" src="img/small/image3.jpg" alt="Third slide"> 
+                </div>
+                <div class="col-md-8 ">
+                  <div class="col-md-12 d-flex justify-content-end">
+                    <h4>$ 2,500</h4>
+                  </div>
+                  <div class="col-md-10">
+                    <table class="table text-center table-borderless">
+                      <thead>
+                        <tr>                         
+                          <th scope="col" style="text-align: justify;">Producto</th>
+                          <th scope="col">Precio</th>
+                          <th scope="col">Cantidad</th>                          
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>                         
+                          <td style="text-align: justify;">Estufa de 4 hornillas ksjdnfd fasdfasjd fasdflaksdf asdfasldfasd fasdfsadfasdf asdfasdfasdf</td>
+                          <td>$2,500</td>
+                          <td><select class="form-select mb-4" aria-label="Default select example">                               
+                                <option selected value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                              </select>
+                          </td>                                                  
+                        </tr>                       
+                      </tbody>
+                    </table>
+                  </div>
+
+                  <div class="col-md-12 d-flex justify-content-end">
+                    <button type="button" class="btn"><i class="bi bi-trash text-danger h3"></i><span class="text-danger"> Eliminar</span></button>                   
+                  </div>
+                </div>
+              </div>              
+            </div>
+          </div><!--fin producto-->
+
+          <!--producto-->
+          <div class="card shadow-sm my-2 rounded-3">
+            <div class="card-body py-1">
+              <div class="row d-flex align-items-center">
+                <div class="col-md-4">
+                  <img class="rounded img-fluid" src="img/small/image3.jpg" alt="Third slide"> 
+                </div>
+                <div class="col-md-8 ">
+                  <div class="col-md-12 d-flex justify-content-end">
+                    <h4>$ 2,500</h4>
+                  </div>
+                  <div class="col-md-10">
+                    <table class="table text-center table-borderless">
+                      <thead>
+                        <tr>                         
+                          <th scope="col" style="text-align: justify;">Producto</th>
+                          <th scope="col">Precio</th>
+                          <th scope="col">Cantidad</th>                          
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>                         
+                          <td style="text-align: justify;">Estufa de 4 hornillas ksjdnfd fasdfasjd fasdflaksdf asdfasldfasd fasdfsadfasdf asdfasdfasdf</td>
+                          <td>$2,500</td>
+                          <td><select class="form-select mb-4" aria-label="Default select example">                               
+                                <option selected value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                              </select>
+                          </td>                                                  
+                        </tr>                       
+                      </tbody>
+                    </table>
+                  </div>
+
+                  <div class="col-md-12 d-flex justify-content-end">
+                    <button type="button" class="btn"><i class="bi bi-trash text-danger h3"></i><span class="text-danger"> Eliminar</span></button>                   
+                  </div>
+                </div>
+              </div>              
+            </div>
+          </div><!--fin producto-->
+
+          <!--producto-->
+          <div class="card shadow-sm my-2 rounded-3">
+            <div class="card-body py-1">
+              <div class="row d-flex align-items-center">
+                <div class="col-md-4">
+                  <img class="rounded img-fluid" src="img/small/image3.jpg" alt="Third slide"> 
+                </div>
+                <div class="col-md-8 ">
+                  <div class="col-md-12 d-flex justify-content-end">
+                    <h4>$ 2,500</h4>
+                  </div>
+                  <div class="col-md-10">
+                    <table class="table text-center table-borderless">
+                      <thead>
+                        <tr>                         
+                          <th scope="col" style="text-align: justify;">Producto</th>
+                          <th scope="col">Precio</th>
+                          <th scope="col">Cantidad</th>                          
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>                         
+                          <td style="text-align: justify;">Estufa de 4 hornillas ksjdnfd fasdfasjd fasdflaksdf asdfasldfasd fasdfsadfasdf asdfasdfasdf</td>
+                          <td>$2,500</td>
+                          <td><select class="form-select mb-4" aria-label="Default select example">                               
+                                <option selected value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                              </select>
+                          </td>                                                  
+                        </tr>                       
+                      </tbody>
+                    </table>
+                  </div>
+
+                  <div class="col-md-12 d-flex justify-content-end">
+                    <button type="button" class="btn"><i class="bi bi-trash text-danger h3"></i><span class="text-danger"> Eliminar</span></button>                   
+                  </div>
+                </div>
+              </div>              
+            </div>
+          </div><!--fin producto--> 
+       
+
+        </div>
+        <div class="col-md-4 my-3">
+          <div class="card my-2 shadow-sm rounded-3 p-0">
+            <div class="card-body px-4">
+               <div class="row">
+                      <table class="table table-borderless table-sm">                     
+                      <tbody >
+                        <tr>                         
+                          <td class="p-0">Subtotal (3 productos):</td> <td class="text-end p-0">$20,000.00</td>                                              
+                        </tr>
+                        <tr >                         
+                          <td class="p-0">Descuento</td> <td class="text-end p-0">$5,000.00</td>                                              
+                        </tr>
+                        <tr>                         
+                          <td class="p-0">Costo de envio</td> <td class="text-end p-0">$150.00</td>                                              
+                        </tr>
+                        <tr>                         
+                          <td colspan="2"><hr></td>                          
+                        </tr>
+
+                        <tr>
+                          <td><b>Total (IVA incluido)</b></td> <td class="text-end text-success">$20,000.00</td>                                              
+                        </tr>
+
+                      </tbody>
+                    </table>                      
+                </div>             
+            </div>
+          </div>
+          * Losprecios estan sujetos a sadaiosjdas
+          <div class="d-grid gap-2 my-3">
+            <button class="btn btn-primary" type="button">Comprar</button>
+            <button type="button" class="btn btn-secondary">Seguir comprando</button>               
+          </div> 
+        </div>
+        
+      </div>      
+   
+    </div>
+   
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="js/menu.js"></script>
+
+   
+  </body>
+</html>
